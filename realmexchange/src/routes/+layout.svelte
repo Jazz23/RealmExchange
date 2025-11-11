7e1c8e0ab62458736b7ee85a7643bfe9c2f6d93d
Jazz23 <noreply>
2025-11-11 04:34:34 +0000

    Add shadcn

--- realmexchange/src/routes/+layout.svelte
+++ realmexchange/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { Button } from '$lib/components/ui/button';
 
 	let { children, data } = $props();
 </script>
@@ -9,12 +10,12 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="flex flex-row justify-end p-3 bg-blue-400">
+<div class="flex flex-row justify-end p-3 bg-blue-400 gap-3">
 {#if data.user}
-	<p class="">Logged in as <strong>{data.user.username}</strong></p>
-	<a class="ml-4" href="/logout">Logout</a>
+	<p class="mt-1">Logged in as <strong>{data.user.username}</strong></p>
+	<Button href="/logout">Logout</Button>
 {:else}
-	<a href="/login">Login</a>
+	<Button href="/login">Login</Button>
 {/if}
 </div>
 
