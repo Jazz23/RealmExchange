7ead6a1a24e80e846a2f35a1bf105a6e71bce02f
Jazz23 <noreply>
2025-11-14 20:47:27 +0000

    Update seeding and devcontainer

--- realmexchange/test/seed-wrangler.sh
+++ realmexchange/test/seed-wrangler.sh
@@ -1,4 +1,9 @@
 echo "Use this script if the database doesn't already exist"
+# Check if ../.wrangler already exists
+if [ -d "../.wrangler" ]; then
+  echo "../.wrangler already exists. Exiting to avoid overwriting existing database."
+  exit 0
+fi
 # Auto enter Y to any prompts
 echo Y | wrangler d1 migrations apply realm-exchange --local --persist-to=../.wrangler
 wrangler d1 execute realm-exchange --local --persist-to=../.wrangler --file=./test/seed-data.sql
